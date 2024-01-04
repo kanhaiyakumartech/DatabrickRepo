45284b6497b59009c5cb9237a7dfc8f47f2c794d
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2024-01-04 19:16:00 +0530

    This is file for employee_silver_to_gold

--- src/Assignment-1/silver_to_gold/employee_silver_to_gold.py
+++ src/Assignment-1/silver_to_gold/employee_silver_to_gold.py
@@ -4,23 +4,46 @@
 
 # COMMAND ----------
 
+
 #8.In gold notebook employee_silver_to_gold, call utils notebook in this notebook Read the table stored in silver layer as dataframe and drop load_date
+# DBTITLE 1,Reading the delta table stored
 
-# Read from Delta table in silver layer
-silver_df = spark.read.format("delta").load("/silver/employee_info/dim_employee")
 
-# Drop load_date
-silver_df = silver_df.drop("load_date")
+filepath = "/silver/employee_info/dim_employee/Employees_table"
+silver_df = spark.read.format("delta").load(filepath)
 
-# 9.find the salary of each department in desc order
-salary_by_department = silver_df.groupBy("department_name").agg({"salary": "max"}).orderBy("max(salary)", ascending=False)
-#10 Add at_load_date
-salary_by_department_add = salary_by_department.withColumn("at_load_date", current_date())
-
-#11.Write the df to dbfs location /gold/employee/table_name(fact_employee) with overwrite and replace where condition on at_load_date
- salary_by_department.write.format("delta").mode("overwrite").option("replaceWhere", "1=1").save("/gold/employee/fact_employee"))
 
 
 # COMMAND ----------
+
+# DBTITLE 1,9finding salary of each department
+from pyspark.sql import functions as F
+
+# Drop load_date column10
+
+silver_df = silver_df.drop("load_date")
+
+# 9 Find salary of each department in desc order
+salary_by_department= silver_df.groupBy("department_name").agg(F.("salary").alias("desc_salary"))
+salary_by_department = salary_by_department.orderBy(F.desc("desc_salary"))
+
+# 10 Add at_load_date
+salary_by_department = salary_by_department.withColumn("at_load_date", F.current_date())
+
+# Update the original DataFrame
+silver_df = salary_by_department
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Salary of each department
+silver_df.display()
+
+# COMMAND ----------
+
+file_format='delta'
+output_path="/gold/employee/fact_employee"
+table_name="fact_employee"
+write_delta(silver_df,file_format,output_path,table_name)
 
 
