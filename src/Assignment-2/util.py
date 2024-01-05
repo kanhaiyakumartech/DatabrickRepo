6fa59cbd745bd736cfec03ed11fcccf84743782b
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2024-01-05 14:19:42 +0530

    This is util file for site_info

--- src/Assignment-2/util.py
+++ src/Assignment-2/util.py
@@ -1,45 +1,28 @@
- from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, LongType
-from pyspark.sql.functions import explode_outer, col, split, lit
-import pyspark.sql.functions as F
+# Databricks notebook source
+import requests
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode_outer, col, split, lit, current_date
 
-def process_data(api_response):
-    spark = SparkSession.builder.getOrCreate()
+def get_api_data(url):
+    return [requests.get(url).text]
 
-    # Create a DataFrame from the API response
-    db = spark.sparkContext.parallelize([api_response])
-    site_info_df = spark.read.option("multiline", True).json(db)
+def read_api_data(spark, url):
+    api_data = get_api_data(url)
+    return spark.read.option("multiline", True).json(spark.sparkContext.parallelize(api_data))
 
-    # Drop unnecessary columns
-    columns_to_drop = ["page", "per_page", "total", "total_pages", "support"]
-    site_info_df = site_info_df.drop(*columns_to_drop)
+def drop_columns(site_info_df, columns_to_drop):
+    return site_info_df.drop(*columns_to_drop)
 
-    # Define the schema for the DataFrame
-    element_schema = StructType([
-        StructField("avatar", StringType(), True),
-        StructField("email", StringType(), True),
-        StructField("first_name", StringType(), True),
-        StructField("id", LongType(), True),
-        StructField("last_name", StringType(), True)
-    ])
+def explode_columns(site_info_df, columns_to_explode):
+    for column in columns_to_explode:
+        site_info_df = site_info_df.withColumn(column, explode_outer(col(f"data.{column}")))
+    return site_info_df.drop("data")
 
-    data_schema = StructType([
-        StructField("data", ArrayType(StructType(element_schema)), True)
-    ])
+def derive_site_address(site_info_df):
+    return site_info_df.withColumn('site_address', split(col('email'), '@').getItem(1))
 
-    # Convert array type to struct type for data
-    df_explode = site_info_df.withColumn("data", explode_outer(col("data")))
-    site_info_df = df_explode.select('data.*')
+def add_load_date(site_info_df):
+    return site_info_df.withColumn('load_date', lit(current_date()))
 
-    # Derive a new column 'site_address' from email with values 'reqres.in'
-    site_info_df = site_info_df.withColumn("site_address", F.lit("reqres.in"))
-
-    return site_info_df
-
-def write_to_delta(df, delta_path, delta_table_name):
-    # Write DataFrame to DBFS location in Delta format with overwrite mode
-    df.write.format("delta").option('mergeschema', True).mode("overwrite").save(delta_path)
-
-    # Create or replace a Delta table in the specified database
-    spark.sql(f"CREATE DATABASE IF NOT EXISTS site_info")
-    df.write.format("delta").option('mergeschema', True).mode("overwrite").saveAsTable(delta_table_name)
+def write_to_delta(site_info_df, delta_path):
+    site_info_df.write.format("delta").mode("overwrite").option("mergeschema", True).save(delta_path)
