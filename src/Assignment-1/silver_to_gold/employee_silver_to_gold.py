839f7c1476a5bef17d75e75f8214efae11e02b4b
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2024-01-05 14:08:02 +0530

    This is file for employee_silver_to_gold

--- src/Assignment-1/silver_to_gold/employee_silver_to_gold.py
+++ src/Assignment-1/silver_to_gold/employee_silver_to_gold.py
@@ -1,49 +1,36 @@
 # Databricks notebook source
-# MAGIC %run "/DataBricksRepo/Assignment-1/source_to_bronze/utils"
-
+# MAGIC %run /Users/ashwathgowda216@gmail.com/DatabricksRepo_Final/source_to_bronze/Utils
 
 # COMMAND ----------
 
-
-#8.In gold notebook employee_silver_to_gold, call utils notebook in this notebook Read the table stored in silver layer as dataframe and drop load_date
 # DBTITLE 1,Reading the delta table stored
-
-
 filepath = "/silver/employee_info/dim_employee/Employees_table"
-silver_df = spark.read.format("delta").load(filepath)
-
-
+employee1_df = spark.read.format("delta").load(filepath)
 
 # COMMAND ----------
 
-# DBTITLE 1,9finding salary of each department
+employee1_df.show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Finnding salary of each department.
 from pyspark.sql import functions as F
 
-# Drop load_date column10
+employee1_df = drop_columns(employee1_df, ["load_date"])
 
-silver_df = silver_df.drop("load_date")
+employee1_df = add_at_load_date(employee1_df, new_column_name="at_load_date")
 
-# 9 Find salary of each department in desc order
-salary_by_department= silver_df.groupBy("department_name").agg(F.("salary").alias("desc_salary"))
-salary_by_department = salary_by_department.orderBy(F.desc("desc_salary"))
-
-# 10 Add at_load_date
-salary_by_department = salary_by_department.withColumn("at_load_date", F.current_date())
-
-# Update the original DataFrame
-silver_df = salary_by_department
-
+employee1_df=order_salary_by_department(employee1_df, salary_column="salary", department_column="department_name")
 
 # COMMAND ----------
 
 # DBTITLE 1,Salary of each department
-silver_df.display()
+employee1_df.display()
 
 # COMMAND ----------
 
+# DBTITLE 1,Writing the delta file
 file_format='delta'
 output_path="/gold/employee/fact_employee"
 table_name="fact_employee"
-write_delta(silver_df,file_format,output_path,table_name)
-
-
+write_delta(employee1_df,file_format,output_path,table_name)
